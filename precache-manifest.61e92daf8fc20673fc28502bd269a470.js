59b807c96674e4c5978791ec2f14ee8a467764f3
kumarnitinsingh <nitin962141@gmail.com>
2020-05-25 18:39:20 +0530

    Updates

---
+++ precache-manifest.61e92daf8fc20673fc28502bd269a470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a10064f16d46bd25a68147bc1401b1",
+    "url": "/getjob/index.html"
+  },
+  {
+    "revision": "047fc37b73f240b8a789",
+    "url": "/getjob/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e0eeb1f0ba38db1883e2",
+    "url": "/getjob/static/css/main.65f1fe29.chunk.css"
+  },
+  {
+    "revision": "047fc37b73f240b8a789",
+    "url": "/getjob/static/js/2.586c3e03.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/getjob/static/js/2.586c3e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0eeb1f0ba38db1883e2",
+    "url": "/getjob/static/js/main.8f00fec7.chunk.js"
+  },
+  {
+    "revision": "1de6c52935e5c8c6ff67",
+    "url": "/getjob/static/js/runtime-main.e53f2cc1.js"
+  }
+]);
